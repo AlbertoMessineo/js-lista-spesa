c2724980f6f0d123f5fbac00500a161d86af3a11
AlbertoMessineo <albevorox@gmail.com>
2023-11-13 17:12:25 +0100

    feat: scaffolfing and some js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const listaSpesa = [ "Pasta per pizza" ,"Passata di pomodoro" , "Mozzarella" , "Prosciutto" , "Ananas" , "Qualche birretta"  , "Pepsi (Meglio della cocacola)" , "Teglia" ];
+
+let i = 0;
+while(i < listaSpesa.length - 1){
+    i++;
+}
